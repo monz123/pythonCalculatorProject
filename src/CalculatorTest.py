42556d97e0986f8d6ba0c06d08cceb12bf23e181
monisha <ms2889@njit.edu>
2020-10-28 22:02:33 -0400

    Task: Adding all the CSVs to a folder

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -9,14 +9,14 @@ class MyTestCase(unittest.TestCase):
         self.calculator = Calculator()
 
     def test_add_method_calculator(self):
-        test_data = CsvReader("src/Unit Test Addition.csv").data
+        test_data = CsvReader("src/CsvForTesting/Unit Test Addition.csv").data
         for row in test_data:
             result = int(row['Result'])
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_sub_method_calculator(self):
-        test_data = CsvReader("src/Unit Test Subtraction.csv").data
+        test_data = CsvReader("src/CsvForTesting/Unit Test Subtraction.csv").data
         for row in test_data:
             result = int(row['Result'])
             self.assertEqual(self.calculator.sub(row['Value 1'], row['Value 2']), result)
